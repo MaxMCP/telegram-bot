34533d60f93a0d88a50f84635352a0cd97e9ef63
unnecessaryuser <unnecessaryuser@t.me>
2022-10-03 03:11:45 +0500

    composer.json update

--- src/Publishable/Providers/TelegramBotServiceProvider.php
+++ src/Publishable/Providers/TelegramBotServiceProvider.php
@@ -8,20 +8,18 @@ class TelegramBotServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->registerConfig();
     }
 
     public function boot()
     {
+        $this->registerConfig();
     }
 
     private function registerConfig()
     {
         $ds = DIRECTORY_SEPARATOR;
-        $configFile = __DIR__ . $ds . '..' . $ds .'config' . $ds .'tg-bot.php';
+        $configFile = __DIR__ . $ds . '..' . $ds . 'config' . $ds . 'tg-bot.php';
 
-        $this->publishes([
-            $configFile => config_path('tg-bot.php')
-        ]);
+        $this->publishes([$configFile => config_path('tg-bot.php')], 'tg-bot-config');
     }
 }
